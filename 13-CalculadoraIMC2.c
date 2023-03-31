cd63c3145f4d3bb44cd6e9a7654db98fca9e97fd
Marcio Dias <67355232+MarcioDias83@users.noreply.github.com>
2023-03-30 21:15:42 -0300

    Update 13-CalculadoraIMC2.c

--- 13-CalculadoraIMC2.c
+++ 13-CalculadoraIMC2.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 
-// Crie um programa que leia altura e massa e calcule o IMC (Índice de Massa Corpórea). Sabendo que a fórmula é: IMC = massa/ (altura*altura).
+// Crie um programa que leia altura e massa e calcule o IMC (Ãndice de Massa CorpÃ³rea). Sabendo que a fÃ³rmula Ã©: IMC = massa/ (altura*altura).
 
 int main()
 {
@@ -18,21 +18,21 @@ int main()
 
 	if (IMC >= 30)
     {
-        printf("O seu IMC é de %.2f. Voce esta obeso.",IMC);
+        printf("O seu IMC Ã© de %.1f. Voce esta obeso.",IMC);
     }
     
     else if ((IMC >= 25) && (IMC < 30))
     {
-        printf("O seu IMC é de %.2f. Voce esta acima do peso.",IMC);
+        printf("O seu IMC Ã© de %.1f. Voce esta acima do peso.",IMC);
     }
     
     else if ((IMC >= 18.5) && (IMC < 25))
     {
-        printf("O seu IMC é de %.2f. Voce esta com o peso normal.",IMC);
+        printf("O seu IMC Ã© de %.1f. Voce esta com o peso normal.",IMC);
     }
     
-    else if (IMC < 18,5)
+    else
     {
-        printf("O seu IMC é de %.2f. Voce esta abaixo do peso.",IMC);
+        printf("O seu IMC Ã© de %.1f. Voce esta abaixo do peso.",IMC);
     }
 }
