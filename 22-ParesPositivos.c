67cbca1aabb74f58ac57b3a12046b6217fab7647
Marcio Dias <67355232+MarcioDias83@users.noreply.github.com>
2023-04-27 19:36:32 -0300

    Update 22-ParesPositivos.c

--- 22-ParesPositivos.c
+++ 22-ParesPositivos.c
@@ -1,34 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
-
-// Crie um programa em C que leia dois números pares e positivos e imprima todos os pares entre eles.
 
 int main()
 {
-	setlocale(LC_ALL, "Portuguese");
+	int numero1, numero2, contador = 0;
 	
-	int numero1;
-	int numero2;
-	int contador = 0;
-	
-	printf("Qual é o primeiro número?\n");
+	printf("Qual e o primeiro numero?\n");
 	scanf("%d",&numero1);
-	printf("Qual é o segundo número?\n");
+	printf("Qual e o segundo numero?\n");
 	scanf("%d",&numero2);
 	
-	while(numero1 <= numero2)
+	numero1+=2; //Acrescenta +2 ao nÃºmero 1 antes de entrar no laÃ§o de repetiÃ§Ã£o
+	
+	while(numero1 < numero2)
 	{
 		if(numero1 < 0 || numero2 < 0){
-			printf("Use somente números positivos!");break;
+			printf("Use somente numeros positivos!");break;
 		}
 	printf("%d\n", numero1);
 	numero1 = numero1 + 2;
 	contador = contador + 1;
 	}
 }
+
 //if (contador == 0)
-//printf("O bloco não foi repetido.");
+//printf("O bloco nao foi repetido.");
 //else
 //printf("O bloco foi repetido %d vezes.", contador);
-
