03a7ddc8f6098673efbd137568c5b6ecef7a0784
Marcio Dias <67355232+MarcioDias83@users.noreply.github.com>
2023-04-27 19:39:43 -0300

    Update 23-SomaPares.c

--- 23-SomaPares.c
+++ 23-SomaPares.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
-// Crie um programa em C que leia dois números pares e positivos e imprima a soma de todos os pares entre eles.
-  
 int main () 
 {
-	setlocale(LC_ALL, "Portuguese");
-	
 	int numero1, numero2, soma = 0;
   
-	printf ("Qual é o primeiro número?\n");
+	printf ("Qual e o primeiro numero?\n");
 	scanf ("%d", &numero1);
-	printf ("\nQual é o segundo número?\n");
+	printf ("\nQual e o segundo numero?\n");
 	scanf ("%d", &numero2);
 
-	while(numero1 <= numero2)
+	numero1 += 2;
+
+	while(numero1 < numero2)
 	{
 		if(numero1 < 0 || numero2 < 0){
-			printf("Use somente números positivos!");break;
+			printf("Use somente numeros positivos!");break;
 		}
 	soma = soma + numero1;
 	numero1 = numero1 + 2;
 	}
-	printf("\nA soma de todos os pares entre primeiro e o segundo numero é %d.", soma);
+	printf("\nA soma de todos os pares entre primeiro e o segundo numero e %d.", soma);
 }
