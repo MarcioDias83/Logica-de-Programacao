0c586e4411974f4ff8dd0cd2e1d7273269d46a16
Marcio Dias <67355232+MarcioDias83@users.noreply.github.com>
2023-04-27 21:09:57 -0300

    Update 25-numeroFatorial.c

--- 25-numeroFatorial.c
+++ 25-numeroFatorial.c
@@ -3,7 +3,7 @@
 
 int main(){
 	
-	int num, fat=1;
+	int num, fat=1;//Fatorial recebe 1, pois caso recebesse 0, ao ser multiplicado pelo número o resultado seria 0.
 	
 	printf("Digite um numero:\n");
 	scanf("%d",&num);
@@ -11,7 +11,7 @@ int main(){
 	while(num>1)//Enquanto o número digitado for maior que 1...
 	{
 		fat*=num;//Fatorial recebe o fatorial * o número digitado
-		num-=1; //Número recebe -1 a cada termino de laço
+		num-=1; //Número recebe -1 a cada término de laço
 }
 printf("O fatorial e: %d.", fat);
 }
