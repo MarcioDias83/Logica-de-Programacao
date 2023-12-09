87cd2a53b290ef255740db047abba8c3b7864297
Marcio Dias <67355232+MarcioDias83@users.noreply.github.com>
2023-12-09 04:16:38 -0300

    teste

--- 02-CelsiusFahrenheit.c
+++ 02-CelsiusFahrenheit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 
-//Crie um programa que leia um valor em graus celsius e converta para fahrenheit. Sabendo que a fórmula é T(°F) = T(°C) × 1.8 + 32.
+//Crie um programa que leia um valor em graus celsius e converta para fahrenheit. Sabendo que a fï¿½rmula ï¿½ T(ï¿½F) = T(ï¿½C) ï¿½ 1.8 + 32.
 
 int main() 
 {
@@ -13,5 +13,5 @@ int main()
 	printf("Informe a temperatura em Celsius:\n");
 	scanf("%f",&tempCelsius);
 	tempFahrenheit = tempCelsius * 1.8 + 32;
-	printf("A temperatura em Fahrenheit é de %.2f.",tempFahrenheit);
+	printf("A temperatura em Fahrenheit serÃ¡ de %.2f.",tempFahrenheit);
 }
